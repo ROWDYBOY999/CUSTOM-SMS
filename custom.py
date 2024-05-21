67929036ed2afe29e51b05c4d548053a91748a59
ROWDYBOY999 <168767690+ROWDYBOY999@users.noreply.github.com>
2024-05-21 20:10:44 +0600

    custom.py

---
+++ custom.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+number = input("Enter the number: ")
+msg = input("Enter the message: ")
+
+url = f'https://shopapp.self-shopping.com/public/selfapi/?sendnumberverifyotp=1&random=0.9304616635066649&contact={number}&otp={msg}'
+
+response = requests.get(url)
+code = response.status_code
+
+response_data = {
+    'telegram': '@rowdyboyx',
+    'status': {
+        'code': code,
+        'api_resp': response.json()
+    }
+}
+
+print(json.dumps(response_data, indent=4))
